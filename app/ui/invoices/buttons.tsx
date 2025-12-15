17934d661f8d90cf2b8dcda71131ef642104bd91
Eduard Gilmutdinov <edgilmutdinov@gmail.com>
2025-12-15 17:28:09 +0000

    Add alternative way to catch the variables

--- app/ui/invoices/buttons.tsx
+++ app/ui/invoices/buttons.tsx
@@ -26,7 +26,13 @@ export function UpdateInvoice({ id }: { id: string }) {
 }
 
 export function DeleteInvoice({ id }: { id: string }) {
-  const deleteInvoiceWithId = deleteInvoice.bind(null, id);
+  // const deleteInvoiceWithId = deleteInvoice.bind(null, id);
+
+  // The alternative way using closure:
+  async function deleteInvoiceWithId() {
+    'use server';
+    await deleteInvoice(id);
+  }
 
   return (
     <form action={deleteInvoiceWithId}>
